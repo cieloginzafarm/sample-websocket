2b6884be5d540ac07d649f7f1c8268de6292603f
Cielo Jordan <cielo@ginzafarm.co.jp>
2020-08-04 17:20:53 +0900

    First init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>Trying out Ayame Lite</h2> 
+    <button v-on:click="sendMessage('You have successfully sent a message')">Send Message</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      connection: null
+    }
+  },
+  methods: {
+    sendMessage: function(message) {
+      console.log(message)
+      console.log(this.connection);
+      this.connection.send(message);
+    }
+  },
+  created: function() {
+    console.log("Starting connection to Ayame Lite Server")
+    this.connection = new WebSocket("wss://ayame-lite.shiguredo.jp/signaling", "cielojordanjp@test-ayame")
+    this.connection.onmessage = function(event) {
+      console.log(event);
+    }
+
+    this.connection.onopen = function(event) {
+      console.log(event)
+      console.log("Successfully connected to the Ayame lite websocket server...")
+    }
+
   }
 }
 </script>
